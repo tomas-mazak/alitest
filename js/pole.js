cc6754516e113eb601410af6844d935b6ab3fc9c
Tomas Mazak <tomas@valec.net>
2021-10-21 15:02:11 +0200

    Nejaky ten javascript

---
+++ js/pole.js
@@ -0,0 +1,7 @@
+//          0       , 1       , 2         , 3
+let pole = ['jablko', 'hruska', 'pomeranc', 'banan'];
+
+ali_oblibene_ovoce = 2;
+
+// prevod z cisla na retezec:
+console.log(pole[ali_oblibene_ovoce]);
